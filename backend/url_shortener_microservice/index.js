e1fe826614677c806f0640ab2ab90a79492b4660
Che Zhu <dev@che-zhu.me>
2024-05-19 07:37:02 +0000

    bug fix

--- backend/url_shortener_microservice/index.js
+++ backend/url_shortener_microservice/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require('dns');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -28,20 +28,26 @@ app.use(express.urlencoded());
 app.post('/api/shorturl', function(req, res) {
   const original_url = req.body["url"];
 
-  if (original_url.startsWith("https://www.")) {
-    const short_url = Object.values(urlData).length;
-
-    urlData[short_url] = original_url;
+  const url_host = new URL(original_url).host;
   
-    res.json({
-      original_url: original_url,
-      short_url: short_url
-    });
-  } else {
-    res.json({
-      error: "invalid url"
-    });
-  }
+  dns.lookup(url_host, (err, address, family) => {
+    console.log(original_url);
+    if (err) {
+      console.log(err, address);
+      res.json({
+        error: "invalid url"
+      });
+    } else {
+      const short_url = Object.values(urlData).length;
+
+      urlData[short_url] = original_url;
+    
+      res.json({
+        original_url: original_url,
+        short_url: short_url
+      });
+    }
+  })
 })
 
 app.get('/api/shorturl/:short_url', function(req, res) {
